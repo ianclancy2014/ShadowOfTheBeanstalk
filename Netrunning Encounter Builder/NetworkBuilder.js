adb116249e00b3d51c7dfce97c8f09a6f5d8f511
ianclancy2014 <61367982+ianclancy2014@users.noreply.github.com>
2023-03-28 12:21:46 -0500

    Added files for the network encounter generator
    Added the index.html and the .js files for the network encounter generator, as well as notes for the program in the .js file

---
+++ Netrunning Encounter Builder/NetworkBuilder.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// This tool is going to be a network generator for Shadow of the Beanstalk.
+// Version 1.0 is going to allow the GM to select a network type (building, remote site, apartment,
+// small office, bar, etc.) select a size (large network, small network, etc.) and difficulty (how
+// generally difficult the ICE protecting the nodes of the network will be)
+//
+// I'm still learning the way running network encounters works in SotB, so this project may
+// be a little wonky as I build it
+//
+/*
+Steps I'm going to take:
+1) Building the basic functionality of the program while I learn the net encounter rules
+2) Put the basic functional pieces together so I can atleast randomly generate a network
+3) Implement the UI
+4) Put the UI and the program together so that the GM only has to use the webpage to generate the network
+4) Touch up the UI
+5) Balance the encounters
+6) Use the program in my games
+
+*/
