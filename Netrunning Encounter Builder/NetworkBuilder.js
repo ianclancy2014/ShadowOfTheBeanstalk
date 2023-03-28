e52bdbe624dc86976a35cbcbc3695ce46187bfc0
ianclancy2014 <61367982+ianclancy2014@users.noreply.github.com>
2023-03-28 15:04:04 -0500

    Added style.css and began work on .js file
    Began the groundwork for the .js file and added style.css as well as got the .html file filled out a bit

--- Netrunning Encounter Builder/NetworkBuilder.js
+++ Netrunning Encounter Builder/NetworkBuilder.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// ***OVERVIEW***
 // This tool is going to be a network generator for Shadow of the Beanstalk.
 // Version 1.0 is going to allow the GM to select a network type (building, remote site, apartment,
 // small office, bar, etc.) select a size (large network, small network, etc.) and difficulty (how
@@ -18,4 +19,43 @@ Steps I'm going to take:
 5) Balance the encounters
 6) Use the program in my games
 
+*/
+
+//randomizes a number between 1 and 10
+let i = Math.floor(Math.random() * 10 + 1);
+console.log(i);
+console.log("Begin Program:");
+
+let systems = [
+  "Lights",
+  "Emergency Lights",
+  "HVAC",
+  "Clean Water",
+  "Sewage",
+  "Grid Power",
+  "Backup Power",
+  "Fire Detection",
+  "Fire Suppression",
+  "Automatic Doors",
+  "Security Doors",
+  "Cameras",
+  "Automated Turrets",
+];
+
+//just testing to see if it can grab one of the items from the systems array
+//console.log(systems[Math.floor(Math.random() * systems.length)]);
+let j = Math.floor(Math.random() * systems.length);
+console.log(j);
+console.log(systems[j]);
+
+//next:
+/*
+1) List basic ICE
+2) List Advanced ICE
+3) List Extreme ICE (if there is any)
+
+4) Create a slider mechanic from 1-5 that pulls from all 3 ICE lists based on the difficulty:
+    ie. at 1, it randomly pulls from just Basic ICE, at 2 it pulls randomly from basic and HAS A CHANCE
+    to pull from Advanced ICE, at 3 it pulls mostly from advanced and sometimes basic, at 4 it pulls from
+    advanced and has a chance to pull from extreme, and at 5 it pulls mainly from extreme
 */
